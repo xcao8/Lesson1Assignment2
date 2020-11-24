2f4db056893279e1c1e237e4caf33ccc8a3a8418
Alexandros Amargianos <alexandros.amargianos@volvocars.com>
2020-11-24 14:51:42 +0100

    Add test post message with timeout side effect

--- tests/test_server_communication_handler.py
+++ tests/test_server_communication_handler.py
@@ -1,8 +1,8 @@
 from unittest.mock import patch
 import json
 from signal_interpreter_client.server_communication_handler import post_message
-
-
+from requests.exceptions import Timeout
+import pytest
 
 @patch("signal_interpreter_client.server_communication_handler.post")
 def test_post_message(mock_post):
@@ -11,6 +11,13 @@ def test_post_message(mock_post):
     mock_post.assert_called_with("http://127.0.0.1:5000/", data=json.dumps({"signal": "11"}),
                                  headers={"content-type": "application/json"})
 
+@patch("signal_interpreter_client.server_communication_handler.post",side_effect=Timeout)
+def test_post_message_with_timeout(mock_post):
+    with pytest.raises(Timeout):
+        post_message("http://127.0.0.1:5000/", {"signal": "11"})
+        mock_post.assert_called_once()
+        mock_post.assert_called_with("http://127.0.0.1:5000/", data=json.dumps({"signal": "11"}),
+                                     headers={"content-type": "application/json"})
 
 
 #@patch("requests.post",return_value="Security Access")
